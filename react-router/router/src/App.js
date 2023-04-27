bb69395bcd728887ac58b0fd4aed53cea6a03e42
James Tang <rhsjamestang2019@gmail.com>
2023-04-26 22:38:28 -0700

    Updated router
    - Added books page
    - Added NewBook page
    - Added NotFound page

--- react-router/router/src/App.js
+++ react-router/router/src/App.js
@@ -1,6 +1,9 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Booklist from "./pages/Booklist";
+import { Book } from "./pages/Book";
+import { NewBook } from "./pages/NewBook"
+import { NotFound } from "./pages/NotFound"
 
 function App() {
   return (
@@ -9,6 +12,7 @@ function App() {
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/books'>Books</Link></li>
+        <li><Link to='/books/new'>NewBook</Link></li>
       </ul>
     </nav>
     
@@ -17,9 +21,15 @@ function App() {
     <Routes>
       <Route path = "/" element = {<Home />}/>
       <Route path = "/books" element = {<Booklist />}/>
+      <Route path = "/books/:id" element = {<Book />}/>
+      <Route path = "/books/new" element = {<NewBook />}/>
+      <Route path = "*" element = {<NotFound />}/>
     </Routes>
     </>
   );
 }
+
+
+// What's the difference between export default and export???
 
 export default App;
---
+++ react-router/router/src/pages/NewBook.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function NewBook(){
+    return(
+        <h1>NewBook</h1>
+    );
+}
+
